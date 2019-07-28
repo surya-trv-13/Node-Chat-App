65a767d7810def7543934f6dc2083d7d0c91bbfc
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2019-07-28 18:34:45 +0530

    Added connect & disconnect event listener

--- server/server.js
+++ server/server.js
@@ -6,7 +6,16 @@ const express = require('express');
 const pathPublic = path.join(__dirname , '../public'); // This will reduce the file path of ../ with the original path...
 const port = process.env.PORT || 1200;
 const app = express();
-var server = http.createServer(app);
+var server = http.createServer(app); // This create a http server initially it was creating server in listen(...) method
+var io = socketIO(server);
+
+io.on('connection',(socket) => {            // This is a built in event listner it will execute when the said event happens then this method calls
+  console.log('New User Connected');
+
+  socket.on('disconnect',() => {
+    console.log('Disconnected User'); // To print the message/or any stuff to execute in the server side after a disconnetion happens in the connection of a user...
+  })
+})
 
 app.use(express.static(pathPublic));
 
